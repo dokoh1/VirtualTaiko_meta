be4e7f885fba7f2cc790d5184617e84e9b4f14db
dokoh1 <146950802+dokoh1@users.noreply.github.com>
2025-04-16 16:37:45 +0900

    시작 메뉴 UI/UX 구현#3

---
+++ Assets/Scripts/UI/System/System.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace dokoh
+{ 
+    public class System : MonoBehaviour
+    {
+        public static SceneManager SceneManager => Instance.sceneManger;
+        [SerializeField] private SceneManager sceneManger;
+        
+        private static System Instance { get; set; }
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+    }
+
+
+
+}
