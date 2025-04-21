d0d5db8971b346918309dba1fa7c786b1f8379b6
TmTree <csh020101@gmail.com>
2025-04-21 15:13:37 +0900

    판정 수정

--- Assets/Scripts/Note/PlayerController.cs
+++ Assets/Scripts/Note/PlayerController.cs
@@ -1,36 +1,70 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private TimingManager timingManager;
-    public Drums drums;
+    // public Drums drums;
     public void Start()
     {
         timingManager = FindFirstObjectByType<TimingManager>();
+        StartCoroutine(HitUpdate());
     }
 
-    void Update()
+    IEnumerator HitUpdate()
     {
-        if (drums.dataSet == DrumDataType.RightFace)
+        while (true)
         {
-            HitResult result = timingManager.CheckTiming();
-
-            switch (result)
+            if (Input.GetKey(KeyCode.Space))
             {
-                case HitResult.Perfect:
-                    Debug.Log("Perfect Hit!");
-                    break;
-                case HitResult.Good:
-                    Debug.Log("Good Hit!");
-                    break;
-                case HitResult.Bad:
-                    Debug.Log("Bad Hit!");
-                    break;
-                case HitResult.Miss:
-                    Debug.Log("Miss...");
-                    break;
+                HitResult result = timingManager.CheckTiming();
+
+                switch (result)
+                {
+                    case HitResult.Perfect:
+                        Debug.Log("Perfect Hit!");
+                        break;
+                    case HitResult.Good:
+                        Debug.Log("Good Hit!");
+                        break;
+                    case HitResult.Bad:
+                        Debug.Log("Bad Hit!");
+                        break;
+                    case HitResult.Miss:
+                        Debug.Log("Miss...");
+                        break;
+                }
+
+                yield return new WaitForSeconds(0.1f);
+            }
+            else
+            {
+                yield return new WaitForSeconds(0.1f);
             }
         }
     }
 }
+    // void Update()
+    // {
+    //     if (drums.dataSet == DrumDataType.RightFace)
+    //     {
+    //         HitResult result = timingManager.CheckTiming();
+    //
+    //         switch (result)
+    //         {
+    //             case HitResult.Perfect:
+    //                 Debug.Log("Perfect Hit!");
+    //                 break;
+    //             case HitResult.Good:
+    //                 Debug.Log("Good Hit!");
+    //                 break;
+    //             case HitResult.Bad:
+    //                 Debug.Log("Bad Hit!");
+    //                 break;
+    //             case HitResult.Miss:
+    //                 Debug.Log("Miss...");
+    //                 break;
+    //         }
+    //     }
+    // }
