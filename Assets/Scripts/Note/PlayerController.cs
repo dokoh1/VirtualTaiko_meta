cf896c3c9c3f4c88fab128fe64bde29c8fa52379
demo <85276514+genmoo@users.noreply.github.com>
2025-04-21 16:39:36 +0900

    판정 범위 픽스

--- Assets/Scripts/Note/PlayerController.cs
+++ Assets/Scripts/Note/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private TimingManager timingManager;
-    // public Drums drums;
+    public Drums drums;
     public void Start()
     {
         timingManager = FindFirstObjectByType<TimingManager>();
@@ -16,25 +16,24 @@ public class PlayerController : MonoBehaviour
     {
         while (true)
         {
-            if (Input.GetKey(KeyCode.Space))
+            if (drums.dataSet == DrumDataType.RightFace)
             {
                 HitResult result = timingManager.CheckTiming();
-
-                switch (result)
-                {
-                    case HitResult.Perfect:
-                        Debug.Log("Perfect Hit!");
-                        break;
-                    case HitResult.Good:
-                        Debug.Log("Good Hit!");
-                        break;
-                    case HitResult.Bad:
-                        Debug.Log("Bad Hit!");
-                        break;
-                    case HitResult.Miss:
-                        Debug.Log("Miss...");
-                        break;
-                }
+                // switch (result)
+                // {
+                //     case HitResult.Perfect:
+                //         Debug.Log("Perfect Hit!");
+                //         break;
+                //     case HitResult.Good:
+                //         Debug.Log("Good Hit!");
+                //         break;
+                //     case HitResult.Bad:
+                //         Debug.Log("Bad Hit!");
+                //         break;
+                //     case HitResult.Miss:
+                //         Debug.Log("Miss...");
+                //         break;
+                // }
 
                 yield return new WaitForSeconds(0.1f);
             }
--- Assets/Scripts/Note/TimingManager.cs
+++ Assets/Scripts/Note/TimingManager.cs
@@ -15,9 +15,9 @@ public class TimingManager : MonoBehaviour
     public Transform Center;
 
     [Header("판정 거리 기준 (중심 기준 거리)")]
-    public float perfectRange = 20f;
-    public float goodRange = 40f;
-    public float badRange = 60f;
+    private float perfectRange = 0.5f;
+    private float goodRange = 1f;
+    private float badRange = 3f;
 
     public HitResult CheckTiming()
     {
@@ -27,14 +27,28 @@ public class TimingManager : MonoBehaviour
         float noteX = note.transform.position.x; // ⬅ 월드 좌표
         float centerX = Center.position.x;        // ⬅ 월드 좌표
         float distance = Mathf.Abs(noteX - centerX);
-
+        // Debug.Log("distance " + distance);
         HitResult result;
-        if (distance <= perfectRange) result = HitResult.Perfect;
-        else if (distance <= goodRange) result = HitResult.Good;
-        else if (distance <= badRange) result = HitResult.Bad;
+        if (distance <= perfectRange) 
+        {   
+            result = HitResult.Perfect;
+        }
+        else if (distance <= goodRange) 
+        {
+
+            result = HitResult.Good;
+        }
+        
+        else if (distance <= badRange)
+        {
+            result = HitResult.Bad;
+        } 
         else result = HitResult.Miss;
 
         Debug.Log($"판정: {result} (Distance: {distance:F1})");
+        Debug.Log("perfectRagne " + perfectRange);
+        Debug.Log("GoodRange" + goodRange);
+        Debug.Log("badRange" + badRange);
 
         if (result != HitResult.Miss)
         {
