5b0520a17d7dcf7d584c4ab2918f8f59c3838187
dokoh1 <146950802+dokoh1@users.noreply.github.com>
2025-04-22 15:42:34 +0900

    DeadGauge 구현#17

--- Assets/Scripts/UI/System/System.cs
+++ Assets/Scripts/UI/System/System.cs
@@ -4,14 +4,23 @@ namespace dokoh
 { 
     public class System : MonoBehaviour
     {
-        public static SceneManager SceneManager => Instance.sceneManger;
-        [SerializeField] private SceneManager sceneManger;
-        
-        private static System Instance { get; set; }
+        public static System Instance { get; private set; }
+
+        public static SceneManager SceneManager => Instance?.sceneManager;
+        [SerializeField] private SceneManager sceneManager;
+
+        public static ScoreManager ScoreManager => Instance?.scoreManager;
+        [SerializeField] private ScoreManager scoreManager;
 
         private void Awake()
         {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject); // 이미 있다면 파괴
+                return;
+            }
             Instance = this;
+            DontDestroyOnLoad(gameObject); // 씬 넘어가도 유지
         }
     }
 }
